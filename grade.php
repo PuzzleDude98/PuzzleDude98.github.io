a7a0e59718cf4eecf8e31a6f32375a0b8c5833fe
PuzzleDude98 <48701078+PuzzleDude98@users.noreply.github.com>
2019-03-25 18:45:27 -0700

    Fix Quiz(Hopfully)

--- grade.php
+++ grade.php
@@ -1,3 +1,14 @@
 <html>
-Score
+    <?php
+    
+    $answer1 = $_POST['question-1-answers'];
+
+    $totalCorrect = 0;
+    
+    if ($answer1 == "B") { $totalCorrect++; }
+
+    
+    echo "<div id='results'>$totalCorrect / 5 correct</div>";
+    
+?>
 </html>
