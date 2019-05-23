ffdbd9dcdf02383e301a0cf3707137382a700b0d
PuzzleDude98 <48701078+PuzzleDude98@users.noreply.github.com>
2019-05-23 08:21:17 -0700

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,4 +7,5 @@ $headers = 'From: website.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
+if(mail($to, $subject, $message, $headers)){echo 'we received your email thank you ';}
 ?>
